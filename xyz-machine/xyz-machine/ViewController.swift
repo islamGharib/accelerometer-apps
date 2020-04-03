6caeb916c8d451045e06d1f3881feeceb991b05d
Islam Gharib <islamgharib@Islams-MacBook-Pro.local>
2020-04-03 18:47:49 +0200

    initial xcode project

---
+++ xyz-machine/xyz-machine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  xyz-machine
+//
+//  Created by Islam Gharib on 4/3/20.
+//  Copyright © 2020 Gharib. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
