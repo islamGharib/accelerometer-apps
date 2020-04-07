165b487e6b6602854047c461d71e3800d3753c9a
Islam Gharib <islamgharib@Islams-MacBook-Pro.local>
2020-04-07 16:56:11 +0200

    limiting the result of accelerometer

--- xyz-machine/xyz-machine/ViewController.swift
+++ xyz-machine/xyz-machine/ViewController.swift
@@ -32,7 +32,15 @@ class ViewController: UIViewController {
     
     func updateLabels(data: CMAccelerometerData?, error: Error?){
         guard let accelerometerData = data else {return}
-        print(accelerometerData)
+        let formatter = NumberFormatter()
+        formatter.minimumFractionDigits = 1
+        formatter.maximumFractionDigits = 1
+        
+        let x = formatter.string(for: accelerometerData.acceleration.x)
+        let y = formatter.string(for: accelerometerData.acceleration.y)
+        let z = formatter.string(for: accelerometerData.acceleration.z)
+        
+        
     }
 
    
