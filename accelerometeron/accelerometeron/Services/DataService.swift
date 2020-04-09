6c2c22fcd7143fae7720cff4a1fbc2ee06eb867d
Islam Gharib <islamgharib@Islams-MacBook-Pro.local>
2020-04-09 23:27:52 +0200

    Configuring image array data

---
+++ accelerometeron/accelerometeron/Services/DataService.swift
@@ -0,0 +1,21 @@
+//
+//  DataService.swift
+//  accelerometeron
+//
+//  Created by Islam Gharib on 4/9/20.
+//  Copyright © 2020 Gharib. All rights reserved.
+//
+
+import UIKit
+
+
+var camera = UIImage(named: "1")!
+var city = UIImage(named: "2")!
+var animals = UIImage(named: "3")!
+var flowers = UIImage(named: "4")!
+var stand = UIImage(named: "5")!
+var urban = UIImage(named: "6")!
+    
+var imageArray = [camera,city,animals,flowers,stand,urban]
+var nameArray = ["CAMERA","CITY","ANIMALS","FLOWERS","STAND","URBAN"]
+
