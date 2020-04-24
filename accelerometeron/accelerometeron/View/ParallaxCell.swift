c0a138220625fec03192862426232976ff731842
Islam Gharib <islamgharib@Islams-MacBook-Pro.local>
2020-04-24 14:29:19 +0200

    Applaing motion effects to itemImageView

--- accelerometeron/accelerometeron/View/ParallaxCell.swift
+++ accelerometeron/accelerometeron/View/ParallaxCell.swift
@@ -14,7 +14,7 @@ class ParallaxCell: UITableViewCell {
     @IBOutlet weak var descriptionLabel: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        setupParallax()
     }
     
     func configuredCell(withImage image: UIImage, andDescription desc: String){
@@ -22,7 +22,27 @@ class ParallaxCell: UITableViewCell {
         descriptionLabel.text = desc
     }
     
+    // applying motion affect
     func setupParallax(){
+        let min = CGFloat(-30)
+        let max = CGFloat(30)
+        
+        // horizontal motion
+        let xMotion = UIInterpolatingMotionEffect(keyPath: "layer.transform.translation.x", type: .tiltAlongHorizontalAxis)
+        xMotion.minimumRelativeValue = min
+        xMotion.maximumRelativeValue = max
+        
+        // vertical motion
+        let yMotion = UIInterpolatingMotionEffect(keyPath: "layer.transform.translation.y", type: .tiltAlongVerticalAxis)
+        yMotion.maximumRelativeValue = min
+        yMotion.maximumRelativeValue = max
+        
+        // grouping both x and y motions
+        let motionEffectGroup = UIMotionEffectGroup()
+        motionEffectGroup.motionEffects = [xMotion,yMotion]
+        
+        // applying motion effect to the itemImageView
+        itemImageView.addMotionEffect(motionEffectGroup)
         
     }
 
